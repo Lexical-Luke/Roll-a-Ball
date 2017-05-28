fdc631aec1d91f21ed15f4a40bfd66ce15570f7f
Luke Wilkinson <lukejamesw@gmail.com>
2017-05-28 02:14:38 +0200

    Its just a ball that rolls arounds and collects rotating squares.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	public Text cntText;
+	public Text winText;
+
+	private Rigidbody rb;
+	private int cnt;
+
+	void Start()
+	{
+		rb = GetComponent<Rigidbody> ();
+		cnt = 0;
+		SetCntText();
+		winText.text = " ";
+	}
+		
+	void FixedUpdate()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rb.AddForce (movement * speed);
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		//Destroy (other.gameObject);
+		if (other.gameObject.CompareTag ("Pick Up")) 
+		{
+			other.gameObject.SetActive (false);
+			cnt++;
+			SetCntText();
+		}
+	}
+
+	void SetCntText()
+	{
+		cntText.text = "Count: " + cnt.ToString ();
+
+		if (cnt >= 11) 
+		{
+			winText.text = "You Win!";
+		}
+	}
+
+}//end
